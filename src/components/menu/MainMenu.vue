bb3b6580b2e68b34cecbf585c5b217aa9305737e
uwriegel <uwriegel@gmail.com>
2019-06-19 23:04:14 +0200

    main menu mouse controlling

--- src/components/menu/MainMenu.vue
+++ src/components/menu/MainMenu.vue
@@ -1,27 +1,104 @@
 <template>
-    <ul id="menubar">
-        <li id="menubar1" tabindex="1">
-            <span class="accelerator">D</span><span>atei</span>
-        </li>
-        <li id="menubar2" tabindex="2">
-            <span class="accelerator">N</span><span>avigation</span>
-        </li>
-        <li id="menubar3" tabindex="3">
-            <span class="accelerator">S</span><span>elektion</span>
-        </li>
-        <li id="menubar4" tabindex="4">
-            <span class="accelerator">A</span><span>nsicht</span>
-        </li>
-    </ul>
+    <div>
+        <div class="flex">
+            <ul id="menubar">
+                <li id="menubar1" tabindex="1" @click="onClick0">
+                    <span class="accelerator">D</span><span>atei</span>
+                </li>
+                <li id="menubar2" tabindex="2" @click="onClick1">
+                    <span class="accelerator">N</span><span>avigation</span>
+                </li>
+                <li id="menubar3" tabindex="3" @click="onClick2">
+                    <span class="accelerator">S</span><span>elektion</span>
+                </li>
+                <li id="menubar4" tabindex="4" @click="onClick3">
+                    <span class="accelerator">A</span><span>nsicht</span>
+                </li>
+            </ul>
+        </div>
+        <SubMenu />
+    </div>
 </template>
 
 <script>
+import SubMenu from './SubMenu.vue'
+
 export default {
-    name: 'MainMenu'
+    name: 'MainMenu',
+    isInitialized: false,
+    isActive: false,
+    keyboardActivated: false,
+    menuBar: null,
+    subMenuOpened: true,
+    components: {
+        SubMenu
+    },
+    methods: {
+        onClick: function (evt, index) {
+            const li = evt.target.closest('li')
+            const selected = li.classList.contains("selected")
+
+            if (!this.isInitialized)
+                this.initialize()
+
+            if (!this.isActive)
+                this.setActive()
+            this.subMenuOpened = true
+
+            this.clearSelection()
+            if (!selected)
+                this.focusLi(li)
+            else
+            {
+                this.close()
+                evt.stopPropagation()
+                evt.preventDefault()
+            }            
+        },
+        initialize: function () {
+            this.menuBar = document.getElementById("menubar")
+            this.menuBar.addEventListener("focusout", evt => {
+                if (!(this.subMenuOpened && this.keyboardActivated) && !this.menuBar.contains(evt.relatedTarget))
+                    this.close()
+            })
+        },
+        focusLi: function (li) {
+            li.classList.add("selected")
+            li.focus()
+            this.isActive = true
+        },
+        setActive: function () {},
+        clearSelection: function () {
+            Array.from(this.menuBar.querySelectorAll("#menubar>li")).forEach(n => n.classList.remove("selected"))
+        },
+        close: function () {
+            // this.closeSubMenus()
+            this.menuBar.classList.remove("keyboardActivated")
+            this.keyboardActivated = false
+            this.clearSelection()
+            this.hasFocus = false
+            this.isActive = false
+            // this.acceleratorInitiated = false
+            // this.setSubMenuClosed()
+            // if (this.openedSubMenu)
+            //    this.openedSubMenu.close()
+            //this.openedSubMenu = null
+            let lis = Array.from(this.menuBar.querySelectorAll("#menubar>li"))
+            lis.forEach(n => n.onmouseover = null)
+//            setTimeout(() => this.focusedView.focus(), 100)
+        },
+        onClick0: function(evt) { this.onClick(evt, 0) },
+        onClick1: function(evt) { this.onClick(evt, 1) },
+        onClick2: function(evt) { this.onClick(evt, 2) },
+        onClick3: function(evt) { this.onClick(evt, 3) }
+    }
 }
 </script>
 
 <style>
+.flex {
+    display: flex;
+}
 #menubar
 {
     user-select: none;
---
+++ src/components/menu/SubMenu.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="test">
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'SubMenu'
+}
+</script>
+
+<style <style scoped>
+    .test {
+        background-color: blue;
+        width: 100px;
+        height: 30px;
+    }
+</style>
+
+
