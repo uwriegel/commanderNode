2b68aa620a1c8b37a8d20c238494ee41aeb8345f
uwriegel <uwriegel@gmail.com>
2019-06-22 21:08:52 +0200

    Columns

--- src/components/controls/Columns.vue
+++ src/components/controls/Columns.vue
@@ -1,15 +1,11 @@
 <template>
     <!-- <thead app-columns [id]="'left'" [columns]="columns" (onSort)="onSort($event)"></thead> -->
     <thead>
-        <th>
-            <td>Name</td>
-        </th>
-        <th>
-            <td>Erw.</td>
-        </th>
-        <th>
-            <td>Datum</td>
-        </th>
+        <tr>
+            <th class="pointer-ew">Name</th>
+            <th class="pointer-ew">Erw.</th>
+            <th class="pointer-ew">Datum</th>
+        </tr>
     </thead>
 </template>
 
@@ -20,5 +16,27 @@ export default {
 </script>
 
 <style scoped>
+    tr th {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        user-select: none;
+        padding-left: 5px;
+        text-align: left;
+        font-weight: normal;
+        color: var(--selected-color);
+        background-color: var(--selected-background-color);
+        border-left-color: white;
+        border-left-style: solid;
+        border-left-width: 1px;
+        transition: background-color 0.3s;
+    }
 
+    tr th:first-child {
+        border-left-width: 0px;
+    }
+
+    th.pointer-ew {
+        cursor: ew-resize;
+    }
 </style>
