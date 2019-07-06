b68fa6e5e345b3cb8bf4b8312dae62d32af9041c
Uwe <uriegel@hotmail.de>
2019-07-06 19:17:59 +0200

    computed

--- src/components/controls/Folder.vue
+++ src/components/controls/Folder.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="root">
         <h1>Der Folder</h1>
-        <table-view :columns='columns' :items='items' :itemHeight='16'></table-view>
+        <table-view :columns='tableViewColumns' :items='items' :itemHeight='16'></table-view>
     </div>
 </template>
 
@@ -29,13 +29,13 @@ export default {
         const path = localStorage[`${this.id}-path`] || "root"
         this.changePath(path)
     },
+    computed: {
+        tableViewColumns() { return this.columns.values }
+    },
     methods: {
         changePath: function (path) {
             this.processor = this.processor.getProcessor(path)
             this.columns = this.processor.getColumns(this.columns)
-
-
-            .values
         }
     }
 }
